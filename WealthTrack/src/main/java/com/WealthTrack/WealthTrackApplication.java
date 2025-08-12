bc32eaaa7e8b439a2e3586edaef4599977f2c658
abhis <abhis@asus8340>
2025-08-12 23:50:29 +0530

    Initial project setup with basic configuration

---
+++ WealthTrack/src/main/java/com/WealthTrack/WealthTrackApplication.java
@@ -0,0 +1,14 @@
+package com.WealthTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WealthTrackApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
