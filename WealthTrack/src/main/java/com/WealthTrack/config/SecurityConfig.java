18eb37281616b4a6969b596975c9542ecdb4560e
abhis <abhis@asus8340>
2025-08-14 13:21:44 +0530

    feat(security): configure stateless security with CORS

---
+++ WealthTrack/src/main/java/com/WealthTrack/config/SecurityConfig.java
@@ -0,0 +1,61 @@
+package com.WealthTrack.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+         
+    
+    	httpSecurity.cors(Customizer.withDefaults())  // Enable CORS
+                .csrf(AbstractHttpConfigurer::disable)  // Disable CSRF
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/status","/health", "/register", "/login").permitAll()
+                		.anyRequest().authenticated())
+                .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+               return httpSecurity.build(); // Build the security chain
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        
+        // Allowed Origins (Frontend URLs)
+        configuration.setAllowedOrigins(List.of("******"));
+        
+        // Allowed HTTP Methods
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        
+        // Allowed Headers
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type","Accept"));
+        
+        // Allow credentials (cookies, tokens)
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        
+        return source;
+    }
+}
